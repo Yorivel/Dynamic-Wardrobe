143d8c29edacd8031430de2e392e3f1c6f2bd8ef
Yorivel <georgios94vel@hotmail.com>
2026-03-01 03:32:29 +0200

    Initial documentation

--- push-to-github.bat
+++ push-to-github.bat
@@ -5,5 +5,5 @@ git add .
 git commit -m "Initial documentation"
 git branch -M main
 git remote add origin https://github.com/Yorivel/Dynamic-Wardrobe.git
-git push -u origin main
+git push -u origin main --force
 pause
